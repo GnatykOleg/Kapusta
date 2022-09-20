5b999c01d5fdbb4dc9f9d85751a6326ca1579104
Oleg Gnatyk <103590840+GnatykOleg@users.noreply.github.com>
2022-09-20 19:43:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import { store, persistor } from 'redux/store';
 import { GoogleOAuthProvider } from '@moeindana/google-oauth';
 import './index.css';
 
-console.log('process.env: ', process.env);
-console.log('process.env.REACT_APP_CLIENT_ID: ', process.env.REACT_APP_CLIENT_ID);
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
